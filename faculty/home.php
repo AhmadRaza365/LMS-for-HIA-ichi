507223afe57c8dbaeb4109c0b4e93eeb5eaf758a
AhmadRaza365 <ahmadrazabhatti30@gmail.com>
2022-02-15 16:07:58 +0500

    Faculty Home Page Setup

--- faculty/home.php
+++ faculty/home.php
@@ -1,1 +1,38 @@
-<h1>Welcome to <?php echo $_settings->info('name') ?></h1>
+<h1 class="my-5 py-3 text-center PrimaryColor">Welcome to HIA-ichi LMS System</h1>
+
+<div class="container">
+
+    <div class="row justify-content-md-center">
+        <div class="col-sm-4">
+            <a href="<?php echo base_url ?>faculty/?page=class">
+                <div class="card card-widget rounded py-3 px-1 SecondaryBg shadow">
+
+                    <h3 class="tertiaryColor mx-auto rounded-top text-center">Manage Classes</h3>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="<?php echo base_url ?>faculty/?page=lesson">
+                <div class="card card-widget rounded py-3 px-1 SecondaryBg shadow">
+
+                    <h3 class="tertiaryColor mx-auto rounded-top text-center">Manage Lessons</h3>
+
+                </div>
+            </a>
+        </div>
+        <div class="col-sm-4">
+            <a href="<?php echo base_url ?>faculty/?page=file_uploads">
+                <div class="card card-widget rounded py-3 px-1 SecondaryBg shadow">
+
+                    <h3 class="tertiaryColor mx-auto rounded-top text-center">Manage Media</h3>
+
+                </div>
+            </a>
+        </div>
+        
+
+    </div>
+
+
+</div>
