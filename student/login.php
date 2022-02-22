ba8d3e44a40bf99db7c7be5a4281b202d615e3e2
AhmadRaza365 <ahmadrazabhatti30@gmail.com>
2022-02-22 17:26:38 +0500

    Student Header, Navbar, footer setup

--- student/login.php
+++ student/login.php
@@ -2,13 +2,13 @@
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
  <?php require_once('inc/header.php') ?>
-<body class="hold-transition login-page bg-purple text-dark">
+<body class="hold-transition login-page PrimaryBg text-dark">
   <script>
     start_loader()
   </script>
 <div class="login-box">
   <!-- /.login-logo -->
-  <div class="card card-outline card-primary">
+  <div class="card card-outline tertiaryBg">
     <div class="card-header text-center">
       <a href="./" class="h1"><b>Login</b></a>
     </div>
@@ -17,28 +17,28 @@
 
       <form id="slogin-frm" action="" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" name="student_id" placeholder="Student ID">
+          <input type="text" class="form-control tertiaryBg" name="student_id" placeholder="Student ID">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-user"></span>
+              <span class="fas fa-user SecondaryColor"></span>
             </div>
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="password" placeholder="Password">
+          <input type="password" class="form-control tertiaryBg" name="password" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+              <span class="fas fa-lock SecondaryColor"></span>
             </div>
           </div>
         </div>
         <div class="row">
           <div class="col-8">
-            <a href="<?php echo base_url ?>">Go to Website</a>
+            <a class="SecondaryColor" href="<?php echo base_url ?>">Go to Website</a>
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block SecondaryBg border-0">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
